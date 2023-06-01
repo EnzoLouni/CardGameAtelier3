76e64398b5c1b7a7dcb17c9127d09bb8725191be
Enzo LOUNI <enzo.louni@worldine.com>
2023-06-01 14:06:59 +0200

    Create Repositories

---
+++ RoomManager/src/main/java/com/emte/roommanage/RoomManageApplication.java
@@ -0,0 +1,13 @@
+package com.emte.roommanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomManageApplication.class, args);
+	}
+
+}
