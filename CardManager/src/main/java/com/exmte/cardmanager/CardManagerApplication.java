e06d4bfd44f36c799e115c437a42310cd6b37be4
Enzo LOUNI <enzo.louni@worldine.com>
2023-06-01 14:15:57 +0200

    Create Repositories 2

--- CardManager/src/main/java/com/exmte/cardmanager/CardManagerApplication.java
+++ CardManager/src/main/java/com/exmte/cardmanager/CardManagerApplication.java
@@ -1,4 +1,4 @@
-package com.exmte.cardmanager;
+package com.emte.cardmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
