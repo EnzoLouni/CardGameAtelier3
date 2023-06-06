277b976ee2ebae91670f5592e9a8ba185abeba1b
NyoHt <thony.fain@gmail.com>
2023-06-06 17:19:43 +0200

    v1 des tests a tester ce soir avec le Zozz

---
+++ Atelier3/store_manager/src/test/java/com/emte/storemanager/StoreControllerTests.java
@@ -0,0 +1,67 @@
+import com.emte.storemanager.controller.StoreController;
+import com.emte.dto.StoreTransactionDto;
+import com.emte.model.StoreOrder;
+import com.emte.storemanager.service.StoreService;
+import com.emte.view.Views;
+import com.fasterxml.jackson.annotation.JsonView;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.validation.Valid;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(StoreController.class)
+public class StoreControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private StoreService storeService;
+
+    @JsonView(Views.StoreView.class)
+    @Test
+    public void testGetTransactions() throws Exception {
+        StoreTransactionDto transactionDto = new StoreTransactionDto();
+        when(storeService.getTransactions()).thenReturn(Collections.singletonList(transactionDto));
+
+        mockMvc.perform(get("/store/transactions"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0]").exists())
+                .andExpect(jsonPath("$[0].action").exists());
+    }
+
+    @Test
+    public void testSell() throws Exception {
+        StoreOrder order = new StoreOrder();
+        when(storeService.sell(any())).thenReturn(true);
+
+        mockMvc.perform(post("/store/sell")
+                .contentType("application/json")
+                .content("{ \"cardId\": 1, \"userId\": 1 }"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"));
+    }
+
+    @Test
+    public void testBuy() throws Exception {
+        StoreOrder order = new StoreOrder();
+        when(storeService.buy(any())).thenReturn(true);
+
+        mockMvc.perform(post("/store/buy")
+                .contentType("application/json")
+                .content("{ \"cardId\": 1, \"userId\": 1 }"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"));
+    }
+}
---
+++ Atelier3/store_manager/src/test/java/com/emte/storemanager/StoreServiceTests.java
@@ -0,0 +1,114 @@
+import com.emte.client.CardClient;
+import com.emte.client.UserClient;
+import com.emte.storemanager.service.StoreService;
+import com.emte.dto.CardDto;
+import com.emte.dto.StoreTransactionDto;
+import com.emte.dto.UserDto;
+import com.emte.mapper.StoreMapper;
+import com.emte.model.StoreAction;
+import com.emte.model.StoreOrder;
+import com.emte.storemanager.dao.StoreRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class StoreServiceTests {
+
+    @MockBean
+    private StoreRepository storeRepository;
+
+    @MockBean
+    private StoreMapper storeMapper;
+
+    @MockBean
+    private CardClient cardClient;
+
+    @MockBean
+    private UserClient userClient;
+
+    @InjectMocks
+    private StoreService storeService;
+
+    @Test
+    public void testGetTransactions() {
+        StoreTransactionDto transactionDto = new StoreTransactionDto();
+        when(storeRepository.findAll()).thenReturn(Collections.singletonList(new StoreTransaction()));
+        when(storeMapper.toStoreTransactionDto(any())).thenReturn(transactionDto);
+
+        List<StoreTransactionDto> transactions = storeService.getTransactions();
+
+        assertEquals(1, transactions.size());
+        assertEquals(transactionDto, transactions.get(0));
+    }
+
+    @Test
+    public void testSell_WithValidOrder() {
+        StoreOrder order = new StoreOrder();
+        CardDto cardSold = new CardDto();
+        UserDto userDto = new UserDto();
+        StoreTransactionDto transactionDto = new StoreTransactionDto();
+
+        cardSold.setUserId(order.getUserId());
+        when(cardClient.getCard(order.getCardId())).thenReturn(cardSold);
+        when(userClient.getUser(order.getUserId())).thenReturn(userDto);
+        when(storeMapper.toStoreTransactionDto(any())).thenReturn(transactionDto);
+
+        boolean sold = storeService.sell(order);
+
+        assertTrue(sold);
+    }
+
+    @Test
+    public void testSell_WithInvalidOrder() {
+        StoreOrder order = new StoreOrder();
+        CardDto cardSold = new CardDto();
+
+        cardSold.setUserId(order.getUserId() + 1); // ID d'utilisateur est différent
+        when(cardClient.getCard(order.getCardId())).thenReturn(cardSold);
+
+        boolean sold = storeService.sell(order);
+
+        assertFalse(sold);
+    }
+
+    @Test
+    public void testBuy_WithValidOrder() {
+        StoreOrder order = new StoreOrder();
+        CardDto cardBought = new CardDto();
+        UserDto newOwner = new UserDto();
+        StoreTransactionDto transactionDto = new StoreTransactionDto();
+
+        cardBought.setUserId(null);
+        when(cardClient.getCard(order.getCardId())).thenReturn(cardBought);
+        when(userClient.getUser(order.getUserId())).thenReturn(newOwner);
+        when(storeMapper.toStoreTransactionDto(any())).thenReturn(transactionDto);
+
+        boolean bought = storeService.buy(order);
+
+        assertTrue(bought);
+    }
+
+    @Test
+    public void testBuy_WithInvalidOrder() {
+        StoreOrder order = new StoreOrder();
+        CardDto cardBought = new CardDto();
+
+        cardBought.setUserId(order.getUserId()); // La carte appartiens déjà a quelqu'un
+        when(cardClient.getCard(order.getCardId())).thenReturn(cardBought);
+
+        boolean bought = storeService.buy(order);
+
+        assertFalse(bought);
+    }
+}
